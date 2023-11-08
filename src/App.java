f4c4418fb49afa497982f18f8832f3b99473f82e
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-11-08 09:59:02 +0100

    uppg1 del A och B klara - la till två whileloopar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //A
+        int varv = 0;
+        while (varv<=5){
+            System.out.println("Varv nummer: "+varv);
+            varv++;
+        }
+
+        //B
+        int varv1 = 2;
+        while (varv1<=5){
+            System.out.println("Varv nummer: "+varv1);
+            varv1++;
+        }
     }
 }
